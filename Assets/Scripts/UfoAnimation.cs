72b7477492a86ab334de054410ce884758ee795f
Asgeir Vinkenes <asgeirv@protonmail.com>
2018-02-07 12:42:27 +0100

    no message

---
+++ Assets/Scripts/UfoAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoAnimation : MonoBehaviour {
+    public GameObject parent;
+    public float rotationSpeed;
+	// Use this for initialization
+	void Start () {
+        rotationSpeed = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(parent.transform.position,Vector3.up,rotationSpeed*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/setUfoThingieTransform.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setUfoThingieTransform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.localPosition = new Vector3(0f, 0.022f, 0.002f);
+        transform.eulerAngles = new Vector3(-90,0,45);
+    }
+}
